02fdbe3c6b20ffc0c13dd5ff5c89c5cd0e044451
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-05-29 13:42:20 +0100

    took out bugged files for deployment

--- cuddlypotato/settings.py
+++ cuddlypotato/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
 
 
 # Default primary key field type
