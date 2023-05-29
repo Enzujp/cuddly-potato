1b4d6e36a5d8c64a1754e179d36bc887bcf86a53
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-05-29 12:35:50 +0100

    deploy ready

--- cuddlypotato/settings.py
+++ cuddlypotato/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+fm^14^m&(!t$%rn6k^tb=fcv&=nvx#do8(mag^9eotc1*tojm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
