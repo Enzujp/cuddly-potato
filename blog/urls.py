7517902a0d5e0936cc8dcb6468a23ab826abbc62
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-06-02 15:59:48 +0100

    reorganized urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
+    path('new-post/', views.new_post, name="new-post"),
     path('<slug:slug>/', views.category, name="category_detail"),
     path('search/', views.search, name="search"),
     path('<slug:category_slug>/<slug:slug>/', views.detail, name="post_detail"),
-    path('new-post/', views.new_post, name="new-post"),
     path('my-posts', views.my_posts, name="my-posts"),
     path('new-post/edit-post/<slug:slug>/', views.edit_post, name="edit-post"),
     path('my-posts/delete-post/<slug:slug>/', views.delete_post, name="delete-post")
